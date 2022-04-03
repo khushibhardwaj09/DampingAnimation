907519a4cfe9ab8b302a08be9f3acc9047b86cff
Khushi Bhardwaj <khushi.bhardwaj@gatech.edu>
2022-04-03 18:27:04 -0400

    Initial Commit

---
+++ DampingAnimation/DampingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DampingAnimationApp.swift
+//  DampingAnimation
+//
+//  Created by  Khushi Bhardwaj on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DampingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
